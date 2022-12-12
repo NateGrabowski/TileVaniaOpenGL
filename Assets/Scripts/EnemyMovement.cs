28accc1102a31745a0c1bb926614c806c1b01d88
Nate Grabowski <nategrabowski@outlook.com>
2022-12-11 19:30:38 -0700

    Slime has movement and bounces between walls

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    [SerializeField] float moveSpeed = 4f;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing() //flips the sprite when hits the wall
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+}
